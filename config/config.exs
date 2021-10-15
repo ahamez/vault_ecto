b288b38442bc278c8d8ebc581072f1a93a56a3b0
Alexandre Hamez <alexandre.hamez@airnity.com>
2021-10-15 21:39:36 +0200

    Simplify configuration

--- config/config.exs
+++ config/config.exs
@@ -3,9 +3,6 @@ import Config
 config :vault_ecto,
   ecto_repos: [VaultEcto.Repo]
 
-config :vault_ecto,
-  secrets_dir: {:env, "VAULT_ECTO_SECRETS_DIR", [default: "./secrets"]}
-
 config :vault_ecto, VaultEcto.Repo,
   pool_size: 10,
   configure: {VaultEcto.Repo, :configure, []},
--- lib/vault_ecto/application.ex
+++ lib/vault_ecto/application.ex
@@ -6,9 +6,10 @@ defmodule VaultEcto.Application do
 
   @impl true
   def start(_type, _args) do
-    conf =
-      %{servers: %{secrets: :secrets}}
-      |> configure()
+    conf = %{
+      servers: %{secrets: :secrets},
+      secrets_dir: System.get_env("VAULT_ECTO_SECRETS_DIR", "./secrets")
+    }
 
     children = [
       {VaultEcto.Secrets, [name: :secrets, conf: conf]},
@@ -17,42 +18,5 @@ defmodule VaultEcto.Application do
 
     opts = [strategy: :one_for_one, name: VaultEcto.Supervisor]
     Supervisor.start_link(children, opts)
-  end
-
-  # -- Private
-
-  defp configure(conf) do
-    conf
-    |> load(:secrets_dir)
-    |> tap(fn conf -> Logger.debug("#{inspect(conf)}") end)
-  end
-
-  defp load(conf, key) do
-    value =
-      case Application.get_env(:vault_ecto, key) do
-        {:env, var, opts} ->
-          {type, opts} = Keyword.pop(opts, :type, :string)
-          {default, opts} = Keyword.pop(opts, :default)
-          {required, _opts} = Keyword.pop(opts, :required, default == nil)
-
-          case {System.get_env(var), required, type} do
-            {nil, true, _type} ->
-              raise "#{var} is required, but unset"
-
-            {nil, false, _type} ->
-              default
-
-            {value, _required, :string} ->
-              value
-
-            {value, _required, :integer} ->
-              String.to_integer(value)
-          end
-
-        value ->
-          value
-      end
-
-    Map.put(conf, key, value)
   end
 end
